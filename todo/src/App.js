408fb3863414031f295ed83253b980855be275ec
Adilsitos <adilsonfrankneia@gmail.com>
2019-11-02 19:40:19 -0300

    add-todo

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,26 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css'
+import AddTodo from './addTodo'
+import Todos from './Todo'
+class App extends Component {
+  state={
+    todos:[
+      {id:1,content:'ir no churras'},
+      {id:2, content:'terminar exercicio react'}
+    ]
+   }
+   deleteTodo = (id) =>{
+    const todos = this.state.todos.filter(todo =>{
+      return todo.id !== id
+    });
+
+    this.setState({
+      todos
+    })
+  }
+
+  insertDoing = (item, texto, botao)=>{
+    var nodeTodo = document.querySelector('#todo1 li')
+    var elementol2 = document.createElement('li')
+    var nodeDoing = document.querySelector('#doing')
+    elementol2.appendChild(texto)
+    elementol2.appendChild(botao)
+    nodeDoing.appendChild(elementol2)
+    
+
+    if(nodeTodo.parentNode){
+        nodeTodo.parentNode.removeChild(item)
+    }
+  }
+
+  addTodo = (todo)=>{
+    todo.id = Math.random();
+    let todos  = [...this.state.todos, todo];
+    this.setState({
+      todos
+    })
+  }
+
+
+   
+  render(){
+
+    
+    return(
+      <div className="App">
+        <h1 className="center blue-text">Todo's</h1>
+        <AddTodo addTodo={this.addTodo}/>
+        <div className="flex-container">
+            <div className="card">
+                <h2 className="card-title">To do</h2>
+                <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+            </div>
+            <div className="card">
+                <h2 className="card-title">Doing</h2>
+                <h2 className="doing"></h2>
+            </div>
+            <div className="card">
+                <h2 className="card-title">Done</h2>
+            </div>
+            
+        </div>
+        
+     
+      </div>
+    );
+  }
+  
 }
+
+const styles = {
+  todoText:{
+    
+  }
+}
+
+
 
 export default App;
