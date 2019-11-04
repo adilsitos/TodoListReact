a48cc87e256092abd52914d8054d16716b257cf5
Adilson <adilsitos@pop-os.localdomain>
2019-11-03 23:20:46 -0300

    add-doing

--- todo/src/pages/todo/index.js
+++ todo/src/pages/todo/index.js
@@ -9,38 +9,60 @@ export default class Todo extends Component{
         info: ""
     }
 
-    addTodo = ()=>{
-        const divTodo = document.getElementById('articleTodo')
-        const valor = document.getElementById("campo").value
-  
-        const {id,info} = this.state
-        var titulo = document.createElement('h1')
-        var texto= document.createTextNode(valor)
-
-        titulo.appendChild(texto)
-        divTodo.appendChild(titulo)
-        
-
-        
+    addDoing = (texto,titulo)=>{
+        const divDoing = document.getElementById('doing')
+        var titulo2 = document.createElement('h1')
+        var botao2 = document.createElement('button')
+       
+        titulo2.appendChild(texto)
+        titulo2.appendChild(botao2)
+        divDoing.appendChild(titulo2)
+        if(titulo.parentNode){
+            titulo.parentNode.removeChild(titulo)
+        }
+       
 
         
     }
 
+
+    addTodo = ()=>{
+        const divTodo = document.getElementById('articleTodo')
+        const valor = document.getElementById("campo").value
+  
+        var botao = document.createElement('button')
+        botao.setAttribute('class','botao2')
+        
+        
+        var titulo = document.createElement('h1')
+        var texto= document.createTextNode(valor)
+        
+        titulo.appendChild(texto)
+        titulo.appendChild(botao)
+        divTodo.appendChild(titulo)
+
+        botao.onclick = () => this.addDoing(texto,titulo)
+    }
+
+    
+
+   
     
     render(){
         return(
             <div className="todo-list">
                 
                 <h1>Todo's</h1>
-                <input type="text" id="campo" value="Inserir"></input>
+                <input type="text" id="campo" ></input>
+                
                 <button onClick={this.addTodo}>Inserir</button>
               <article id="articleTodo">
                   <h4>Todo</h4>
               </article>
-              <article>
+              <article id="doing">
                   <h4>Doing</h4>
               </article>
-              <article>
+              <article  id="done">
                   <h4>Done</h4>
               </article>
             </div>
