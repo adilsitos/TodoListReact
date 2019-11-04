f493ad31cf6dea2c9ef02d794d91f1d4ddbc0910
Adilson <adilsitos@pop-os.localdomain>
2019-11-03 22:56:05 -0300

    add-todo-function

--- todo/src/pages/todo/index.js
+++ todo/src/pages/todo/index.js
@@ -4,15 +4,37 @@ import './styles.css'
 
 export default class Todo extends Component{
     
+    state = {
+        id: 0,
+        info: ""
+    }
+
+    addTodo = ()=>{
+        const divTodo = document.getElementById('articleTodo')
+        const valor = document.getElementById("campo").value
+  
+        const {id,info} = this.state
+        var titulo = document.createElement('h1')
+        var texto= document.createTextNode(valor)
+
+        titulo.appendChild(texto)
+        divTodo.appendChild(titulo)
+        
+
+        
+
+        
+    }
+
     
     render(){
         return(
             <div className="todo-list">
                 
                 <h1>Todo's</h1>
-                <input type="text"></input>
-                <button>Inserir</button>
-              <article>
+                <input type="text" id="campo" value="Inserir"></input>
+                <button onClick={this.addTodo}>Inserir</button>
+              <article id="articleTodo">
                   <h4>Todo</h4>
               </article>
               <article>
